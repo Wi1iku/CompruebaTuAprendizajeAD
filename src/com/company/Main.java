336a4e90a45edb6f29cb0328c9e256c18bf7063e
Wi1iku <oscarchudy984@gmail.com>
2021-11-16 10:24:54 +0100

    first commit, ej fin

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        File departamentos = new File("Departamentos.dat");
+        DataOutputStream datosbin = new
+                DataOutputStream(new FileOutputStream(departamentos));
+
+        //Declaracion departamentos
+        int[] numerodeps = {10,20,30,40,50,60};
+        String [] nombredeps ={"Contabilidad","Finanzas","Recursos Humanos","Departamento 4","Departamento5","Departamento6"};
+        String [] localidaddeps ={"Madrid","Barcelona","Mentrida","Merida","Jerez de la frontera","Cadiz"};
+
+        for (int i = 0; i <numerodeps.length ; i++) {
+            datosbin.writeInt(numerodeps[i]);
+            datosbin.writeUTF(nombredeps[i]);
+            datosbin.writeUTF(localidaddeps[i]);
+        }
+        System.out.println("Archivo dat creado/actualizado");
+        datosbin.close();
+
+    }
+}

17241a9a843e8b6b8309f63319c9fb10a3f048cb
Wi1iku <oscarchudy984@gmail.com>
2021-11-16 10:00:04 +0100

    ej finalizado

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,34 @@
 package com.company;
 
+import java.io.File;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        String parametro="";
+        String parametro;
         System.out.println("Borrador de directorios 3000");
-        if (parametro.equals("")){
-        System.out.println("Introduce el nombre del directorio a borrar");
+        try{
+        parametro=args[0];}
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Introduce el nombre del directorio a borrar");
+            Scanner teclao = new Scanner(System.in);
+            parametro = teclao.next();
+        }
+
+        try {
+            File archivo = new File(parametro);
+            if(archivo.delete()){
+                System.out.println("El archivo "+archivo.getName()+" ha sido eliminado");
+            }else{
+                System.out.println("No se ha podido eliminar el archivo");
+            }
+        }
+        catch (NullPointerException e){
+            System.out.println("Error, archivo no encontrado");
+        }catch (Exception e){
+            System.out.println("Error inesperado");
         }
 
     }

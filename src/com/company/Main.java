23df628ce7c39eb548cfa983347a7cd65fd00d06
Wi1iku <oscarchudy984@gmail.com>
2021-11-16 08:50:22 +0100

    firstcmmit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String parametro="";
+        System.out.println("Borrador de directorios 3000");
+        if (parametro.equals("")){
+        System.out.println("Introduce el nombre del directorio a borrar");
+        }
+
+    }
+}

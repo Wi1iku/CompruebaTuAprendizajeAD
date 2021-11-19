2ca6a74b3518a93419ac9e619372c668f4cbec43
Wiku/LesWikes/Wi1iku <oscarchudy984@gmail.com>
2021-11-19 16:38:10 +0100

    final commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,12 +11,11 @@ import javax.xml.transform.stream.*;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        int id;
-        int departamento;
+        int deps;
         int posicionfich=0;
-        char[] aplld= new char[10];
-        double salario;
-        File file = new File("Empleados.dat");
+        char[] nombre= new char[15];
+        char [] locali= new char[15];
+        File file = new File("Departamentos.dat");
         RandomAccessFile acceso = new RandomAccessFile(file, "r");
 
 
@@ -25,29 +24,36 @@ public class Main {
         try {
             DocumentBuilder xmlcrear = granja.newDocumentBuilder();
             DOMImplementation implementacion = xmlcrear.getDOMImplementation();
-            Document  documentoxml = implementacion.createDocument(null, "Empleados", null);
+            Document  documentoxml = implementacion.createDocument(null, "Departamentos", null);
             documentoxml.setXmlVersion("1.0");
             boolean check=true;
             while (check){
                 acceso.seek(posicionfich);
-                id=acceso.readInt();
-                for (int i = 0; i < aplld.length ; i++) {
-                    aplld[i]=acceso.readChar();
-                }
-                String aplldString = new String(aplld);
-                departamento = acceso.readInt();
-                salario = acceso.readDouble();
+                deps=acceso.readInt();
+                System.out.println(acceso.getFilePointer()+"departamentp");
 
-                if (id>=1){
+                for (int i = 0; i < nombre.length ; i++) {
+                    nombre[i]=acceso.readChar();
+                }
+                String nombreString = new String(nombre);
+                System.out.println(acceso.getFilePointer()+"apellido");
+
+                for (int i = 0; i < locali.length ; i++) {
+
+                    locali[i]=acceso.readChar();
+                }
+                System.out.println(acceso.getFilePointer()+"loclaiad");
+                System.out.println(acceso.length()+"totalfichero");
+                String localiString = new String(locali);
+                if (deps>=1 && deps%10==0){
                     Element raiz = documentoxml.createElement("empleado");
                     documentoxml.getDocumentElement().appendChild(raiz);
+                    xmlentrada("id", Integer.toString(deps), raiz, documentoxml);
+                    xmlentrada("nombre", nombreString.trim(), raiz, documentoxml);
+                    xmlentrada("localidad", localiString.trim(), raiz, documentoxml);
 
-                    xmlentrada("id", Integer.toString(id), raiz, documentoxml);
-                    xmlentrada("apellido", aplldString.trim(), raiz, documentoxml);
-                    xmlentrada("departamento", Integer.toString(departamento), raiz, documentoxml);
-                    xmlentrada("salario", Double.toString(salario), raiz, documentoxml);
                 }
-                posicionfich=posicionfich+36;
+                posicionfich=posicionfich+64;
                 if (acceso.getFilePointer() == acceso.length()){
                     check=false;
                 }
